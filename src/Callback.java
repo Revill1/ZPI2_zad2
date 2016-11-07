d6cc6038abd35f9e05597a42f2f8182cbd7f1246
Pelka <wojciech.pelka@accenture.com>
2016-11-07 23:28:06 +0100

    callback class added

---
+++ src/Callback.java
@@ -0,0 +1,29 @@
+
+public class Callback implements ICallback
+{
+
+	@Override
+	public boolean getFlag() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getPath() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setFlag(boolean flag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPath(boolean path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
